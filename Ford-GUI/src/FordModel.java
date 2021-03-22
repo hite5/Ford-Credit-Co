ebbd641877a652c95d33f658425c01cc8d496ee2
Henry Hite <hohite@uh.edu>
2021-03-21 21:35:37 -0500

    GUI is fully implemented and has working exception handling.

--- Ford-GUI/src/FordModel.java
+++ Ford-GUI/src/FordModel.java
@@ -6,12 +6,31 @@ import java.awt.event.ActionListener;
 public class FordModel {
     private JButton submit;
     private JPanel mainPanel;
+    private JLabel FordLogo;
+    private JLabel modelLabel;
+    private JTextField customerInput;
+    private int answer;
+    private String message;
 
     public FordModel() {
         submit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                try {
+                    answer = Integer.parseInt(customerInput.getText());
+                    if (answer % 3 == 0 && answer % 5 == 0) {
+                        message = "MustangBronco";
+                    } else if (answer % 3 == 0) {
+                        message = "Mustang";
+                    } else if (answer % 5 == 0) {
+                        message = "Bronco";
+                    } else {
+                        message = customerInput.getText();
+                    }
+                    JOptionPane.showMessageDialog(null, message);
+                } catch (NumberFormatException exp){
+                    JOptionPane.showMessageDialog(null, "Not a valid input!");
+                }
             }
         });
     }
@@ -20,9 +39,10 @@ public class FordModel {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Ford Model Selector");
         frame.setContentPane(new FordModel().mainPanel);
-        frame.setPreferredSize(new Dimension(550,400));
+        frame.setPreferredSize(new Dimension(320,155));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
+
 }
